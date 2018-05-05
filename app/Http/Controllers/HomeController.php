eab7149d9fdd352fbde55fd41d07534e21ed4028
rinda <rk662@njit.edu>
2018-05-05 15:47:01 -0700

    Increase pagination size

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
     public function index()
     {
         $user = Auth::user();
-        $questions = $user->questions()->paginate(6);
+        $questions = $user->questions()->paginate(18);
         return view('home')->with('questions', $questions);
     }
 }
